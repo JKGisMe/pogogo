51479ebf6ec8de3dbf27d39470e81175474863b4
Kate <(none)>
2016-07-31 19:18:47 -0400

    should be able to register and login now

--- app/routes/auth/register.js
+++ app/routes/auth/register.js
@@ -6,7 +6,10 @@ export default Ember.Route.extend({
   },
   actions: {
     doRegister(){
-      console.info('Do Register');
+      this.get('currentModel').save()
+        .then(() => {
+          this.transitionTo('auth.login');
+        });
     }
   }
 });
