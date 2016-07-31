03f4248d2fbc4272554c36db537f39766fb8aa42
Kate Godzicki <(none)>
2016-07-31 18:51:04 -0400

    client validation

--- app/routes/auth/register.js
+++ app/routes/auth/register.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.createRecord('user');
+  },
   actions: {
     doRegister(){
-      console.info("Do Register");
+      console.info('Do Register');
     }
   }
 });
